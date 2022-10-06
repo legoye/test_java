efa56b5e9c91d33dc8f33fee19fd8887a0140a08
LEGOYE <goyo.sicni@gmail.com>
2022-10-06 16:52:42 -0500

    service adde

---
+++ src/main/java/com/test/model/UserRequest.java
@@ -0,0 +1,56 @@
+package com.test.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+public class UserRequest {
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Pattern(regexp = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/",
+            message = "Email must be valid")
+    private String email;
+    @Pattern(regexp = "^[a-zA-Z0-9]{8}", message = "Length must be 8 characters")
+    private String password;
+
+    private List<Phone> phones;
+
+    public UserRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+}
---
+++ src/main/java/com/test/model/UserResponse.java
@@ -0,0 +1,69 @@
+package com.test.model;
+
+import java.time.LocalDateTime;
+
+public class UserResponse {
+
+    private long id;
+
+    private LocalDateTime created;
+
+    private LocalDateTime modified;
+
+    private LocalDateTime lastLogin;
+
+    private String token;
+
+    private boolean isActive;
+
+    public UserResponse() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
